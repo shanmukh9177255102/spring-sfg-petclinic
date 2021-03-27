7079f2454b24503a05f2a989afed5071390a0dad
shannu73 <mullapudishanmukh@outlook.com>
2021-03-27 13:02:26 +0530

    Added Basic POJO models

---
+++ src/main/java/com/springpetclinic/petclinic/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.springpetclinic.petclinic.sfgpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/springpetclinic/petclinic/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.springpetclinic.petclinic.sfgpetclinic.model;
+
+public class Vet extends Person{
+}
