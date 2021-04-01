573a51279463524699dfcd698ca0f082aa807cd1
shannu73 <mullapudishanmukh@outlook.com>
2021-04-01 12:33:13 +0530

    added list view for Owners, Vets

--- pet-clinic-web/src/main/java/com/springpetclinic/petclinic/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springpetclinic/petclinic/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,23 @@
 package com.springpetclinic.petclinic.sfgpetclinic.controllers;
 
+import org.springframewor.sfpetclinicdata.services.OwnersService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnersService ownersService;
+
+    public OwnerController(OwnersService ownersService) {
+        this.ownersService = ownersService;
+    }
+
     @RequestMapping({"/","/index","/index.html"})
-    public String listOwners(){
+    public String listOwners(Model m){
+        m.addAttribute("owners",ownersService.findAll());
         return "owners/index";
     }
 }
--- pet-clinic-web/src/main/java/com/springpetclinic/petclinic/sfgpetclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/com/springpetclinic/petclinic/sfgpetclinic/controllers/PetController.java
@@ -1,13 +1,21 @@
 package com.springpetclinic.petclinic.sfgpetclinic.controllers;
 
+import org.springframewor.sfpetclinicdata.services.PetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PetController {
+    private final PetService petService;
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
 
     @RequestMapping({"/pets","/pets/index","/pets/index.html"})
-    public String listPets(){
+    public String listPets(Model m){
+        m.addAttribute("pets",petService.findAll());
         return "pets/index";
     }
 }
