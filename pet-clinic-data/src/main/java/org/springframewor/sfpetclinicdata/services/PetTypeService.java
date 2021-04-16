f52e9896cc522f5f4dfa90e2742f3a0572e61742
shannu73 <mullapudishanmukh@outlook.com>
2021-04-16 10:38:34 +0530

    added pet type service

---
+++ pet-clinic-data/src/main/java/org/springframewor/sfpetclinicdata/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package org.springframewor.sfpetclinicdata.services;
+
+import org.springframewor.sfpetclinicdata.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/org/springframewor/sfpetclinicdata/services/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package org.springframewor.sfpetclinicdata.services.map;
+
+import org.springframewor.sfpetclinicdata.model.PetType;
+import org.springframewor.sfpetclinicdata.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+}
