504f61d0ba19e13d39f9e726dc63c87794abc357
shannu73 <mullapudishanmukh@outlook.com>
2021-04-27 17:14:52 +0530

    added JPA Entity to Visit

--- pet-clinic-data/src/main/java/org/springframewor/sfpetclinicdata/model/Visit.java
+++ pet-clinic-data/src/main/java/org/springframewor/sfpetclinicdata/model/Visit.java
@@ -1,11 +1,20 @@
 package org.springframewor.sfpetclinicdata.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name="visits")
 public class Visit extends BaseEntity{
 
+    @Column(name="date")
     private LocalDate date;
+
+    @Column(name="description")
     private String decription;
+
+    @ManyToOne
+    @JoinColumn(name="pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
