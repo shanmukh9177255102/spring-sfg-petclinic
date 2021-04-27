0d675457a82cfcddcd7a204595cb4e23beb9b037
shannu73 <mullapudishanmukh@outlook.com>
2021-04-27 16:54:10 +0530

    added JPA Entity to Owner

--- pet-clinic-data/src/main/java/org/springframewor/sfpetclinicdata/model/PetType.java
+++ pet-clinic-data/src/main/java/org/springframewor/sfpetclinicdata/model/PetType.java
@@ -1,7 +1,16 @@
 package org.springframewor.sfpetclinicdata.model;
 
+import jdk.jfr.Enabled;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity{
 
+    @Column(name = "name")
     private String name;
 
     public String getName() {
